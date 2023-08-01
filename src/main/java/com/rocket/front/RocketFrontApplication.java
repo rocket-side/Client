53f25932bf4799b7e16bd7314d1cf00fdeed8fa4
jeongm <jeongm0220@gmail.com>
2023-08-01 14:14:07 +0900

    rocket-side init

---
+++ src/main/java/com/rocket/front/RocketFrontApplication.java
@@ -0,0 +1,13 @@
+package com.rocket.front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocketFrontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rocket/front/RocketFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocket.front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
