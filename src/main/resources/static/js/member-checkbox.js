764f6b5ab7f83193f308e8c64c44df9b46c93b8b
ansdmswl0722 <ansdmswl0722@gmail.com>
2023-08-15 22:45:10 +0900

    project-style.css에 모든 프로젝트 관련 css 붙임 delete recuit-read.css

--- src/main/resources/static/js/member-checkbox.js
+++ src/main/resources/static/js/member-checkbox.js
@@ -1,21 +1,14 @@
-const editAllButton = document.querySelector('.edit-all-button');
-const editButtons = document.querySelectorAll('.edit-button');
+document.addEventListener('DOMContentLoaded', function() {
+    const editAllButton = document.querySelector('.edit-all-button');
+    const teamMemberCards = document.querySelectorAll('.card-body.members'); // 모든 팀원 카드
 
-editAllButton.addEventListener('click', () => {
-    const checkboxContainers = document.querySelectorAll('.checkbox-container');
+    editAllButton.addEventListener('click', () => {
+        teamMemberCards.forEach(card => {
+            const deleteButton = card.querySelector('.delete-button');
+            deleteButton.classList.toggle('d-none');
+        });
 
-    checkboxContainers.forEach(container => {
-        container.classList.toggle('d-none');
-    });
-
-    editButtons.forEach(button => {
-        button.nextElementSibling.classList.add('d-none');
-    });
-});
-
-editButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const checkboxContainer = button.parentElement.querySelector('.checkbox-container');
-        checkboxContainer.classList.toggle('d-none');
+        const buttonText = editAllButton.textContent;
+        editAllButton.textContent = buttonText === '수정' ? '확인' : '수정';
     });
 });
